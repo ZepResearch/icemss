7a18ab11fe450b5679e2ae7b20a4ea1f14988e2b
ShardulVanage <vsharadul@gmail.com>
2025-01-09 14:36:29 +0530

    change content

--- components/Co-OrganiztionLogo.jsx
+++ components/Co-OrganiztionLogo.jsx
@@ -42,7 +42,7 @@ export default function CoOrganizationLogos() {
     
       <div className="container mx-auto px-4 md:px-6">
         <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-4xl text-center sm:text-center mb-8 underline underline-offset-2 decoration-primary dark:decoration-yellow-50">
-          Our Co-organizations
+        Co-Organizers
         </h2>
         <CoOrganizationLogosClient logos={logos} />
       </div>
