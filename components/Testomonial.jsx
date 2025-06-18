db991a25885fab27fba042b15a1442448ee4b7d7
ShardulVanage <vsharadul@gmail.com>
2025-06-18 12:41:10 +0530

    Update testimonial avatar: replace Unsplash image with local file

--- components/Testomonial.jsx
+++ components/Testomonial.jsx
@@ -76,7 +76,7 @@ const testimonials = [
       "I had wonderful experience with Zep Research during the recent conference in Kuala Lumpur, Malaysia. The staff is soo cooperative and helpful. I had an amazing time and great learning      experience with them.	",
     name: "Dr. Aishwarya Singh",
     title: "Professor of Engineering, Stanford University",
-    avatar: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    avatar: "/testimonial/t1.jpg",
   },
   {
     quote:
