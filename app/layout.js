0b3b5e7780f899b18a5ad890e5397281299a2d8c
ShardulVanage <vsharadul@gmail.com>
2025-08-23 01:03:37 +0530

    feat: Add Zep Research analytics script to layout

--- app/layout.js
+++ app/layout.js
@@ -71,6 +71,11 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
+          <script
+          async
+          src="https://analytics.zepresearch.com/script.js"
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+        ></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased mt-12`}
