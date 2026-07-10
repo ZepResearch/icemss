b8f0da5c18edee8e016e77194fe5170be3d27bb1
ShardulVanage <vsharadul@gmail.com>
2026-07-10 09:23:59 +0530

    fix: comment out TicketCTA component in Home page

--- app/page.js
+++ app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
       <SpeakerSection/>
       <SDGSection/>
       <CTASection/> 
-      <TicketCTA/>
+      {/* <TicketCTA/> */}
       <Testimonial/>
       <VenueCTA/>
       <FAQ/>
